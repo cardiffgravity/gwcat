2b1d869abb9f3994ab06f74e53ee2b7f867c42b3
Chris North <chris.north@astro.cf.ac.uk>
2019-05-03 16:03:31 +0100

    started gracedb code

--- python/gwcat/gwcat_mod.py
+++ python/gwcat/gwcat_mod.py
@@ -161,31 +161,6 @@ def gwosc2cat(gwoscIn):
 
     return(catOut)
 
-def getSupereventList():
-    from ligo.gracedb.rest import GraceDb,GraceDbBasic
-    service_url = 'https://gracedb.ligo.org/api/'
-    client = GraceDbBasic(service_url)
-
-    # Retrieve an iterator for events matching a query.
-    events = client.superevents('far < 1.0e-4')
-    # For each event in the search results, add the graceid
-    # and chirp mass to a dictionary.
-    results = {}
-    for event in events:
-        sid = event['superevent_id']
-        results[sid]={id:event['superevent_id']}
-        results[sid]['far']=event['far']
-        filename = 'bayestar.fits'
-        print('downloading {} for superevent {}'.format(filename,sid))
-        clFits=GraceDbBasic(service_url)
-        outFilename = '{}_{}'.format(sid,filename)
-        fout=open(outFilename,'w')
-        r = clFits.files(sid,filename)
-        fout.write(r.read())
-        fout.close()
-
-    return(results)
-
 
 class GWCat(object):
     def __init__(self,fileIn='../data/events.json'):
