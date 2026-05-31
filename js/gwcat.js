af1bff28ad142c57242b9370d05ee4808c318dfc
Chris North <chris.north@astro.cf.ac.uk>
2026-05-31 19:22:58 +0100

    fixed variable scope issue

--- js/gwcat.js
+++ js/gwcat.js
@@ -642,20 +642,21 @@ GWCat.prototype.getLink = function(event,ltype='',ltxt='',lfile=''){
                     let skymapMeta = this.getCatalogMeta('skymap_files');
                     console.log('skymapMeta:', skymapMeta);
                     if (skymapMeta && skymapMeta.filename) {
-                        if (ltype.indexOf('thumb') >=0)
+                        let filename = skymapMeta.filename;
+                        if (ltype.indexOf('thumb') >=0){
                             if (skymapMeta.filename_thumb) {
                                 console.log('Using metadata thumbnail template:', skymapMeta.filename_thumb);
                                 // Replace placeholders in filename
-                                let filename = skymapMeta.filename_thumb;
+                                filename = skymapMeta.filename_thumb;
                             }else{
                                 console.log('Adapting metadata template for thumbnail (.png > .thumb.png):', skymapMeta.filename);
                                 // Replace placeholders in filename
-                                let filename = skymapMeta.filename.replace('.png', '.thumb.png');
+                                filename = filename.replace('.png', '.thumb.png');
                             }
                         }else{
                             console.log('Using metadata template:', skymapMeta.filename);
                             // Replace placeholders in filename
-                            let filename = skymapMeta.filename;
+                            filename = skymapMeta.filename;
                         }
                         // Get base-url from specific link type, fall back to top-level, then link URL
                         let baseUrl = '';
